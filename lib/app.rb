52803c30e56d5e5d6651451f1563aee996b0a609
Weldys Santos <weldyss@gmail.com>
2013-08-03 00:22:19 -0300

    Criando fila para apenas um caixa, sem classificação

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,40 @@
 class Queue
 
   attr_reader :booth, :clients
+  attr_accessor :line
 
   def initialize(booth, clients)
     @booth = booth
     @clients = clients 
+    @line = Array.new(clients) {[:k, :v]}
   end
 
+  def more_than_20
+    create_queues if @booth > 1
+    time_in_the_queue
+  end
+
+  private
+
+  def time_in_the_queue
+    count_time = 0
+    behind = [0, 0, 0] 
+    @line.to_enum.with_index do |client, c|
+      attendance = 0
+      for j in 0..c-1
+        attendance += @line[j][1]
+      end
+      total_time = (attendance - client[0])
+      count_time += 1 if total_time > 20
+      behind = [client[0], client[1], total_time]
+    end
+    count_time
+  end
+
+  def create_queues
+    queues = Array.new(@booth)
+    @line.each do |arrive, duration|
+      
+    end
+  end
 end
--- spec/lib/app_spec.rb
+++ spec/lib/app_spec.rb
@@ -3,20 +3,67 @@ require_relative '../../lib/app'
 
 describe Queue do
 
-  subject {Queue.new(1, 5)}
+  subject { Queue.new(1, 5) }
 
   its(:booth) { should eq 1 }
   its(:clients) { should eq 5 }
 
-  context 'configuring queue' do
-    it 'should indicate how many minutes client spends'
-    it 'should indicate when clients arrived'
+  context 'configuring' do
+
+    subject { Queue.new(1, 3) }
+
+    it 'with clients' do
+      expect(subject.line).to have(3).itens
+    end
+
+    it 'should indicate how many minutes client spends' do
+      subject.line.each do |c| 
+        c[0] = 0
+        c[1] = 10
+      end
+      expect(subject.line.first[0]).to eq 0
+      expect(subject.line.first[1]).to eq 10
+    end
   end
 
   context 'processing' do
-    it 'must choose a queue to client'
-    it 'must return what queue'
-    it 'must return time in the queue'
-    it 'must return how many spent more than 20 minutes'
+    context 'how many spent more than 20 minutes' do
+
+      it 'with one booth' do
+        queue = Queue.new(1,5)
+        queue.line = [
+          [ 0, 10],
+          [ 0, 10],
+          [ 1, 10],
+          [ 2, 10],
+          [30, 10]
+        ]
+        expect(queue.more_than_20).to eq 1
+      end
+
+      it 'with more than one booth' do
+        pending 'Falta implementar com mais de um caixa'
+        queue = Queue.new(3, 16)
+        queue.line = [
+          [ 0, 10],
+          [ 0, 10],
+          [ 0, 10],
+          [ 3, 10],
+          [ 5, 10],
+          [ 7, 10],
+          [11, 10],
+          [13, 10],
+          [14, 10],
+          [15, 10],
+          [16, 10],
+          [17, 10],
+          [18, 30],
+          [19, 10],
+          [20, 10],
+          [23, 30]
+        ]
+        expect(queue.more_than_20).to eq 2
+      end
+    end
   end
 end
